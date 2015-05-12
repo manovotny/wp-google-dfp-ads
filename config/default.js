096411ea8d7c35c1fdd734db8efc8084abdce3fa
Michael Novotny <manovotny@gmail.com>
2015-05-11 22:22:14 -0500

    1.0.1 release.

--- config/default.js
+++ config/default.js
@@ -28,7 +28,7 @@ module.exports = (function () {
             slug: 'wp-google-dfp-ads',
             type: 'plugin', // Should be `plugin` or `theme`.
             url: 'https://github.com/manovotny/wp-google-dfp-ads',
-            version: '1.0.0'
+            version: '1.0.1'
         }
     };
 
--- src/classes/class-wp-google-dfp-ads.php
+++ src/classes/class-wp-google-dfp-ads.php
@@ -24,7 +24,7 @@ class WP_Google_DFP_Ads {
      *
      * @var string
      */
-    protected $version = '1.0.0';
+    protected $version = '1.0.1';
 
     /* Public
     ---------------------------------------------------------------------------------- */
